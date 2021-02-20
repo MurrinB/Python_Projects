b3065b5b3f082dd89a509ff7f6556dee34c71257
MurrinB <britneemurrin@yahoo.com>
2021-02-20 12:51:19 -0700

    placed parameters in a function and called it with argument

--- Practice Assignments/functions.py
+++ Practice Assignments/functions.py
@@ -1,12 +1,16 @@
 
-mySentence = "I love the color"
+mySentence = " loves the color"
 
 color_list = ['red', 'blue', 'green', 'pink', 'teal', 'black']
 
-def color_function():
+def color_function(name):
+    lst = []
     for i in color_list:
-        print("{} {}".format(mySentence, [i]))
+        msg = "{0} {1} {2}".format(name,mySentence,i)
+        lst.append(msg)
+    return lst
 
 
-
-color_function()
+lst = color_function('Britnee')
+for i in lst:
+    print(i)
