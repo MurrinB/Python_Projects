70a1d6a9790a1cb4d7bc4792a9492b8d4efdac6e
MurrinB <britneemurrin@yahoo.com>
2021-02-27 16:03:58 -0700

    Created DB and used Python to edit DB

--- Assignments/sqlite3TXT.py
+++ Assignments/sqlite3TXT.py
@@ -1,3 +1,30 @@
+# Create a database and use python to communicate with sqlite3 to preform a query on a DB
 
 import sqlite3
 
+conn = sqlite3.connect('assignment.db') # Create new DB
+
+with conn :
+    cur = conn.cursor() # Connect to DB to create a table
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_assignment( \
+        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+        col_file TEXT \
+        )")
+    conn.commit() # commit changes to DB
+conn.close() # close connection with DB 
+
+# create tuple of file names
+fileList = ('information.docx', 'Hello.txt', 'myImage.png', \
+            'myMovie.mpg', 'World.txt', 'data.pdf', 'myPhoto.jpg')
+
+
+conn = sqlite3.connect('assignment.db') # open connection 
+# loop through each object in the tuple for the .txt files
+for txt in fileList:
+    if txt.endswith('.txt'):
+        with conn:
+            cur = conn.cursor()
+            # Add .txt files to tbl_assignment 
+            cur.execute("INSERT INTO tbl_assignment (col_file) VALUES (?)", (txt,))
+            print(txt)
+conn.close() 
