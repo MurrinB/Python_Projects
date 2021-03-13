5d9156a7e3e11dc5209d969c4d209d33a7640967
MurrinB <britneemurrin@yahoo.com>
2021-03-13 14:50:17 -0700

    Create replica of given GUI

---
+++ Assignments/Gui_Challenge/gui_challenge.py
@@ -0,0 +1,31 @@
+#
+# Create simple window with tkinter thats has 4 buttons and two input areas
+#
+# Author: Britnee Murrin
+#
+
+from tkinter import *
+import tkinter as tk
+
+import gui_challenge_gui
+
+# Create window
+class ParentWindow(Frame):
+    def __init__ (self, master, *args, **kwargs): 
+        Frame.__init__ (self, master, *args, **kwargs)
+
+        # Frame configuration
+        self.master = master
+        self.master.minsize(500,200)
+        self.master.maxsize(500,200)
+        self.master.title("Check Files")
+
+        # load gui widgets from a seperate module
+        gui_challenge_gui.load_gui(self)
+
+
+# establish order for Python to run code        
+if __name__ == "__main__":
+    root = tk.Tk()
+    App = ParentWindow(root)
+    root.mainloop()
---
+++ Assignments/Gui_Challenge/gui_challenge_gui.py
@@ -0,0 +1,37 @@
+#
+# Create simple window with tkinter thats has 4 buttons and two input areas
+#
+# Author: Britnee Murrin
+#
+
+from tkinter import *
+import tkinter as tk
+
+import gui_challenge
+
+def load_gui(self):
+    # Create button and location
+    self.btn_browse1 = tk.Button(self.master,width=12,height=1,text='Browse...')
+    self.btn_browse1.grid(row=1,column=0,padx=(25,0),pady=(50,5),sticky=S+W)
+    
+
+    self.btn_browse2 = tk.Button(self.master,width=12,height=1,text='Browse...')
+    self.btn_browse2.grid(row=2,column=0,padx=(25,0),pady=(5,5),sticky=S+W)
+
+    self.btn_check = tk.Button(self.master,width=12,height=2,text='Check for files...')
+    self.btn_check.grid(row=3,column=0,rowspan=2,padx=(25,0),pady=(5,50),sticky=S+W)
+
+    self.btn_close = tk.Button(self.master,width=12,height=2,text='Close Program')
+    self.btn_close.grid(row=3,column=5,rowspan=2,padx=(25,5),pady=(5,50),sticky=S+E)
+
+    # Create input spaces
+    self.txt_browse1 = tk.Entry(self.master,width=50,text='')
+    self.txt_browse1.grid(row=1,column=1,columnspan=5,padx=(25,5),pady=(50,0))
+
+    self.txt_browse2 = tk.Entry(self.master,width=50,text='')
+    self.txt_browse2.grid(row=2,column=1,columnspan=5,padx=(25,5),pady=(0,0))
+                          
+
+if __name__ == '__main__':
+    pass
+
